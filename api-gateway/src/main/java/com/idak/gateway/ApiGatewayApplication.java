063db13ad48cf9953e88600b8d9a912860780559
AKADI <akadi@hachette-livre.fr>
2017-03-06 21:47:12 +0100

    add api-gateway project

---
+++ api-gateway/src/main/java/com/idak/gateway/ApiGatewayApplication.java
@@ -0,0 +1,18 @@
+package com.idak.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * Created by akadi on 04/03/2017.
+ */
+@EnableDiscoveryClient
+@EnableZuulProxy
+@SpringBootApplication
+public class ApiGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+}
